bea23a90a8282c5a92a003be9a30ea3e6c66639d
Dhinesh-Dhinesh <dhineshrandy953@gmail.com>
2021-11-16 19:55:39 +0530

    pg

--- pages/story.js
+++ pages/story.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -6,11 +6,27 @@ import {
   StyleSheet,
 } from 'react-native';
 
-const Story = ({navigation}) => {
+const Story = () => {
+    const [pass,setPass] = useState('');
+
+    function pg () {
+        const charset = 'aEFGHI7bcdhi01NOPXYZ2$3fklpqrsQRSTuv%emnofgwxy4#56zABCD89JKUVWtLM';
+        const max = 12;
+        var pass = '';
+
+        for (let i = 0; i <= max; i++) {
+            const rval = Math.floor(Math.random() * charset.length + 1);
+            pass += charset.charAt(rval);
+        }
+
+        setPass(pass);
+    }
+
     return(
         <View style={styles.cent}>
-            <Text style={styles.txt}>Story Page</Text>
-            <Button title="go back" onPress={()=>navigation.goBack()}/>
+            <Text style={styles.txt}>Password Generator</Text>
+            <Text style={{fontSize:25}}>{pass}</Text>
+            <Button title="Generate Pass" onPress={pg}/>
         </View>
     );
 }
